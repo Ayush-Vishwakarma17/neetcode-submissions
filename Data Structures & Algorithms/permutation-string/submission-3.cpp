3cb9b4ce614f5e609c789c53d496035c74eaff86
Ayush-Vishwakarama <158470245+Ayush-Vishwakarma17@users.noreply.github.com>
2026-09-26 10:43:06 +0530

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s2.size() < s1.size()) {
+            return false;
+        }
+        vector<int> f1(26, 0);
+        vector<int> f2(26, 0);
+        for (auto &c : s1) {
+            f1[c-'a']++;
+        }
+        int k = s1.size();
+        int l = 0, r = 0;
+
+        while (r < k) {
+            f2[s2[r] - 'a']++;
+            r++;
+        }
+        r--;
+        while (r < s2.size()) {
+            if (f1 == f2) {
+                return true;
+            } 
+            f2[s2[l] - 'a']--;
+            l++;
+            r++;
+            if (r < s2.size()) {
+                f2[s2[r] - 'a']++;
+            }
+            
+            if (f1 == f2) {
+                return true;
+            } 
+            
+        }
+        return false;
+    }
+};
